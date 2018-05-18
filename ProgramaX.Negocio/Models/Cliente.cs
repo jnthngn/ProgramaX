7e63ef416038ff1c695e64ffad9d5419d1ce4180
jnthngn <38336375+jnthngn@users.noreply.github.com>
2018-05-18 19:19:56 -0300

    Adiciona informações

--- ProgramaX.Negocio/Models/Cliente.cs
+++ ProgramaX.Negocio/Models/Cliente.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace ProgramaX.Negocio.Models
 {
-    class Cliente
+    public class Cliente
     {
+        
     }
 }
--- ProgramaX.Tela/Program.cs
+++ ProgramaX.Tela/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgramaX.Negocio.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace ProgramaX.Tela
     {
         static void Main(string[] args)
         {
+            Cliente cliente = new Cliente();
         }
     }
 }
